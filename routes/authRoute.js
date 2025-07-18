f4f9a4cd1de0bb1b7367931a0092e15c3875a561
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-07-18 21:06:50 +0545

    feat: add logout
    Signed-off-by: Ashim-Stha <ashimshrestha2384@gmail.com>

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,14 @@
 "use strict";
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+  logout,
+} = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/logout", logout);
 
 module.exports = router;

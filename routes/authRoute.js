08dbbc629270acdfabb93f792b03f2e81438d79f
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-07-18 21:03:38 +0545

    feat: add login
    Signed-off-by: Ashim-Stha <ashimshrestha2384@gmail.com>

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,9 @@
 "use strict";
 const express = require("express");
-const { registerUser } = require("../controllers/authController");
+const { registerUser, loginUser } = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
